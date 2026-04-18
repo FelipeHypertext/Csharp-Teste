8dd9156fb4ecbbee946c6a2bebef8bf99a77ca74
Reparadoxo <joaofe.mokdse@gmail.com>
2026-04-18 16:57:33 -0300

    Initial commit and first exercise

---
+++ segundoMaior/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace segundoMaior;
+
+class Program
+{
+    static void Main()
+    {
+        /*Criação e chamada da função aqui
+        Teste rápido:
+        var lista = new List<int> { 3, 2, 1, 5, 5, 4 };
+        var resultado = SegundoMaior(lista);
+        Console.WriteLine(resultado);
+        */
+    }
+    public static int? SegundoMaior(List<int> listaNumeros)
+    {
+        if (listaNumeros == null || listaNumeros.Distinct().Count() < 2)
+        {
+            return null;
+        }
+
+        int? maior = null;
+        int? segundoMaior = null;
+
+        /*
+        Verificação caso a lista esteja vazia ou ela tenha 2 ou menos elementos iguais.
+            */
+
+        foreach (int num in listaNumeros)
+        {
+            if (maior == null || num > maior) 
+            {
+                if (maior != null && num != maior) {
+                    segundoMaior = maior;
+                }
+                maior = num;
+            }
+            else if (num != maior && (segundoMaior == null || num > segundoMaior))
+            {
+                    segundoMaior = num; 
+            }
+        }
+        return segundoMaior;
+    }
+}
